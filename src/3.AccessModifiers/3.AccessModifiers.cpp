1b15839a795d4677223d76715859ab6971d21a09
Евгений <rausch08@yandex.ru>
2023-03-30 22:05:08 +0300

    Модификаторы доступа

---
+++ src/3.AccessModifiers/3.AccessModifiers.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+/**
+* Модификаторы доступа
+*/
+
+/// <summary>
+/// Структура класса Point.
+/// </summary>
+class Point {
+public:
+    int x;
+
+    void Print() {
+        cout << "y: " << y << "\nx: " << x << "\nz: " << z << endl;
+        PrintY();
+    }
+
+private:
+    int y;
+    int z;
+
+    void PrintY() {
+        cout << y << endl;
+        cout << "Приватный метод PrintY" << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru_RU");
+
+    Point point;
+    point.Print();
+
+}
