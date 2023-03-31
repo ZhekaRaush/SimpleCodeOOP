54ccfc6653dd460d85f1160163fd53241b1d7be7
Евгений <rausch08@yandex.ru>
2023-03-31 18:19:20 +0300

    Инкапсуляция

---
+++ src/5.Encapsulation/5.Encapsulation.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/**
+* Инкапсуляция.
+*/
+
+/// <summary>
+/// Кофемолка.
+/// </summary>
+class CoffeGrinder {
+private:
+    /// <summary>
+    /// Напряжение сети.
+    /// </summary>
+    /// <returns>Булевое значение. TRUE или FALSE</returns>
+    bool CheckVoltage()
+    {
+        return true;
+    }
+
+public:
+    /// <summary>
+    /// Метод Start
+    /// </summary>
+    void Start()
+    {
+        if (CheckVoltage())
+        {
+            cout << "Кофемолка мелет кофе." << endl; // Запускаем работу кофемолки
+        }
+        else
+        {
+            cout << "Низкий уровень напряжения, кофемолка не запускается." << endl; // Кофемолка не работает
+        }
+        
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru_RU");
+
+    CoffeGrinder coffeGrinder; // Экземпляр класса кофемолка.
+
+    coffeGrinder.Start(); // Запускаем работу скрипта.
+
+    return 0;
+}
