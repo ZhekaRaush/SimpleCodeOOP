0122b675101b3a79bd9404f06458766406162a28
Евгений <rausch08@yandex.ru>
2023-03-31 19:16:50 +0300

    Конструктор.

---
+++ src/6.Constructor/6.Constructor.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/**
+* Конструктор.
+*/
+
+/// <summary>
+/// Необходимо установить значение X и Y для экземпляра класса.
+/// </summary>
+class Point
+{
+public:
+	/// <summary>
+	/// Конструктор.
+	/// </summary>
+	/// <param name="valueX">Целочисленное значение параметра X</param>
+	/// <param name="valueY">Целочисленное значение параметра Y</param>
+	Point(int valueX, int valueY)
+	{
+		x = valueX; // Параметр X
+		y = valueY; // Параметр Y
+	}
+
+    /// <summary>
+    /// Метод для вывода в консоль.
+    /// </summary>
+    void Print()
+    {
+        cout << "X = " << x << "\t" << "Y = " << y << endl << endl;
+    }
+
+private:
+	int x;
+	int y;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru_RU");
+
+	Point point(7, 12);
+    point.Print();
+
+	Point pointB(77, 46);
+	pointB.Print();
+
+    return 0;
+}
